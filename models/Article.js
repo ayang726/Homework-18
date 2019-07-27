c1b0b0908934a69d02228a75b23d2336ca47abf7
AwesomePresent <yoursmile128@hotmail.com>
2019-07-27 10:42:58 -0400

    Initial structures and scrapping

---
+++ models/Article.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+
+});
+
+module.exports = mongoose.model("Article", schema);
